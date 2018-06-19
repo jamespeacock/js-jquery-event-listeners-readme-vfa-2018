1ad77994f1e0de4e5b93becd7d2231a05ac82989
James Peacock <james.hoke.peacock@gmail.com>
2018-06-19 17:07:20 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,11 +6,9 @@ $('h1').on("eventname", function(){
 function submitIt() {
   $("form").on("submit", function() {
     if ($( "input:first" ).val() === "correct") {
-      alert('your form is going to be submitted now');
+      alert('Your form is going to be submitted now.');
       return;
     }
-    alert("you entered the wrong value");
-    return;
   });
 }
 
